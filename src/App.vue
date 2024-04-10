b26d70eb9b7cf574fca2df05f44815f628561fb0
evasyukov <egor.vasyukov1@gmail.com>
2024-04-10 11:53:31 +0700

    create Date.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
-
+  <div class="left-menu">
+    <Date />
+  </div>
 </template>
 
 <script>
+import Date from "./components/Date.vue"
 
+export default {
+  components: {
+    Date,
+  },
+}
 </script>
+
+<style>
+.left-menu {
+  width: 30vw;
+  height: 100%;
+  background-color: #353d54;
+  border-radius: 0 20px 20px 0;
+  box-shadow: 4px 0px 10px 0px rgba(0, 0, 0, 0.25);
+}
+</style>
---
+++ src/components/Date.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="date-block">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
