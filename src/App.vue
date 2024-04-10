e4ea11293fd455d77965fe79b65d769b004193ae
evasyukov <egor.vasyukov1@gmail.com>
2024-04-10 16:08:57 +0700

    fix style

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,12 @@ export default {
 
 <style>
 .left-menu {
-  width: 30vw;
+  width: 34.5vw;
   height: 100%;
   background-color: #353d54;
   border-radius: 0 20px 20px 0;
   box-shadow: 4px 0px 10px 0px rgba(0, 0, 0, 0.25);
+  gap: 0px;
+  opacity: 0px;
 }
 </style>
