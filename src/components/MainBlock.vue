5150c0e7234deb7c90959b6f0f087fa6e5b84969
evasyukov <egor.vasyukov1@gmail.com>
2024-04-12 12:40:16 +0700

    add icon for menu

--- src/components/MainBlock.vue
+++ src/components/MainBlock.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="menu" :class="{ visible: isVisible}">
-    <router-link to="/start-work" class="button" @click="changeVisible()">Начать работу</router-link>
-    <router-link to="/notifications" class="button" @click="changeVisible()">Уведомления</router-link>
-    <router-link to="/training" class="button" @click="changeVisible()">Обучение</router-link>
-    <router-link to="/achievements" class="button" @click="changeVisible()">Достижения</router-link>
+  <div class="menu" :class="{ visible: isVisible }">
+    <router-link to="/start-work" class="button" @click="changeVisible()">
+      <h1>Начать работу</h1>
+      <img src="/assets/start-work.svg" alt="" />
+    </router-link>
+    <router-link to="/notifications" class="button" @click="changeVisible()">
+      <h1>Уведомления</h1>
+      <img src="/assets/messange.svg" alt="" />
+    </router-link>
+    <router-link to="/training" class="button" @click="changeVisible()"
+      ><h1>Обучение</h1>
+      <img src="/assets/training.svg" alt=""
+    /></router-link>
+    <router-link to="/achievements" class="button" @click="changeVisible()"
+      ><h1>Достижения</h1>
+      <img src="/assets/achievements.svg" alt=""
+    /></router-link>
   </div>
   <router-view></router-view>
 </template>
@@ -13,14 +25,14 @@ export default {
   name: "MainBlock",
   data() {
     return {
-      isVisible: false // флаг видимости
+      isVisible: false, // флаг видимости
     }
   },
   methods: {
     changeVisible() {
       this.isVisible = !this.isVisible
-    }
-  }
+    },
+  },
 }
 </script>
 
@@ -44,5 +56,4 @@ export default {
   text-decoration: none;
   font-family: Montserrat;
 }
-
 </style>
